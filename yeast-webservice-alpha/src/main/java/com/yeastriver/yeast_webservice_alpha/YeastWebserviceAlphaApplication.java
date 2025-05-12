fa4994a5c8bb717fa3fb94626d6bab175b92d806
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-05-12 17:07:25 +0900

    Initial commit

---
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/yeast_webservice_alpha/YeastWebserviceAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.yeastriver.yeast_webservice_alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeastWebserviceAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeastWebserviceAlphaApplication.class, args);
+	}
+
+}
---
+++ yeast-webservice-alpha/src/test/java/com/yeastriver/yeast_webservice_alpha/YeastWebserviceAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeastriver.yeast_webservice_alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeastWebserviceAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
