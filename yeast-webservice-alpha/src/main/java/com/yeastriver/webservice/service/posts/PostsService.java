496779a1e7dcd42e90d4d489aebe53db7e4eb5f6
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-06-17 10:32:30 +0900

    JPA 활용하여 등록기능 추가

---
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.yeastriver.webservice.service.posts;
+
+import com.yeastriver.webservice.domain.posts.PostsRepository;
+import com.yeastriver.webservice.web.dto.PostsSaveRequestDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
