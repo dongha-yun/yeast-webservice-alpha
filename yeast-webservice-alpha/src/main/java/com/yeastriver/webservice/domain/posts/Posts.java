0eaede619c38d28b85510f8fa549e9e4be5bca71
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-06-17 23:55:37 +0900

    JPA 활용하여 등록일시/수정일시 자동 입력되도록

--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/domain/posts/Posts.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.yeastriver.webservice.domain.posts;
 
+import com.yeastriver.webservice.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
