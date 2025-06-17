e6450b2553cd95ee2aa9cee2da87f253ccfc0af2
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-06-17 22:45:40 +0900

    JPA 활용하여 수정/조회기능 추가

--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/service/posts/PostsService.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yeastriver.webservice.service.posts;
 
+import com.yeastriver.webservice.domain.posts.Posts;
 import com.yeastriver.webservice.domain.posts.PostsRepository;
+import com.yeastriver.webservice.web.dto.PostsResponseDto;
 import com.yeastriver.webservice.web.dto.PostsSaveRequestDto;
+import com.yeastriver.webservice.web.dto.PostsUpdateRequestDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/web/PostsApiController.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.yeastriver.webservice.web;
 
 import com.yeastriver.webservice.service.posts.PostsService;
+import com.yeastriver.webservice.web.dto.PostsResponseDto;
 import com.yeastriver.webservice.web.dto.PostsSaveRequestDto;
+import com.yeastriver.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +18,20 @@ public class PostsApiController {
             @RequestBody PostsSaveRequestDto requestDto
     ) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(
+            @PathVariable Long id,
+            @RequestBody PostsUpdateRequestDto requestDto
+    ) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById(
+            @PathVariable Long id
+    ) {
+        return postsService.findById(id);
     }
 }
