00ef0599b0a2555301dd9ed4ce6e8ea9aff7cad6
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-06-18 16:19:00 +0900

    mustache 게시글 등록, 수정화면 개발

--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/domain/posts/PostsRepository.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.yeastriver.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa 에서 제공하지 않는 메소드를 쿼리로 작성할 경우
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/service/posts/PostsService.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.yeastriver.webservice.service.posts;
 
 import com.yeastriver.webservice.domain.posts.Posts;
 import com.yeastriver.webservice.domain.posts.PostsRepository;
+import com.yeastriver.webservice.web.dto.PostsListResponseDto;
 import com.yeastriver.webservice.web.dto.PostsResponseDto;
 import com.yeastriver.webservice.web.dto.PostsSaveRequestDto;
 import com.yeastriver.webservice.web.dto.PostsUpdateRequestDto;
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +40,10 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // 조회 속도가 개선되므로 등록, 수정, 삭제 기능 전혀 없을때 적용
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
 }
