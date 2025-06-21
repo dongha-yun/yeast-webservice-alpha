f5477ee7a3660e4de9d0349dbb57f6e2f1de5cea
dongha.yun@kakao.com <dongha.yun@kakao.com>
2025-06-21 18:03:26 +0900

    loginUserArgumentResolver 문제 해결

--- yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/config/WebConfig.java
+++ yeast-webservice-alpha/src/main/java/com/yeastriver/webservice/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.yeastriver.webservice.config;
 
 import com.yeastriver.webservice.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +13,12 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final LoginUserArgumentResolver loginUserArgumentResolver;
+    private LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Autowired
+    public void setLoginUserArgumentResolver(LoginUserArgumentResolver loginUserArgumentResolver) {
+        this.loginUserArgumentResolver = loginUserArgumentResolver;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
